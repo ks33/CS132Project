9a59225c1badbe472bf87f140cf808a581a665a0
JessieFeng <jiaxi_feng@brown.edu>
2014-04-09 20:30:12 -0400

    return classifier

--- src/main/java/WekaProcessing.java
+++ src/main/java/WekaProcessing.java
@@ -14,7 +14,7 @@ import weka.gui.visualize.PlotData2D;
 import weka.gui.visualize.ThresholdVisualizePanel;
 
 public class WekaProcessing {
-    public static void main(String[] args) throws Exception {
+    public static Classifier train(String[] args) throws Exception {
         // load CSV file and convert to Arff file
         CSVLoader loader = new CSVLoader();
         loader.setSource(new File("Data.csv"));
@@ -36,40 +36,41 @@ public class WekaProcessing {
         eval.crossValidateModel(myClassifier, train, 10, new Random(1));
         System.out.println(eval.toSummaryString("\nResults\n==========\n", true));
         System.out.println("Precision is: " + eval.precision(1));
+        return myClassifier;
         
-        // generate curve
-        ThresholdCurve tc = new ThresholdCurve();
-        int classIndex = 0;
-        Instances result = tc.getCurve(eval.predictions(), classIndex);
-        
-        // plot curve
-        ThresholdVisualizePanel vmc = new ThresholdVisualizePanel();
-        vmc.setROCString("(Area under ROC = " +  Utils.doubleToString(tc.getROCArea(result), 4) + ")");
-        vmc.setName(result.relationName());
-        PlotData2D tempd = new PlotData2D(result);
-        tempd.setPlotName(result.relationName());
-        tempd.addInstanceNumberAttribute();
-        // specify which points are connected
-        boolean[] cp = new boolean[result.numInstances()];
-        for (int n = 1; n < cp.length; n++)
-            cp[n] = true;
-        tempd.setConnectPoints(cp);
-        // add plot
-        vmc.addPlot(tempd);
-        
-        // display curve
-        String plotName = vmc.getName();
-        final javax.swing.JFrame jf =
-        new javax.swing.JFrame("Weka Classifier Visualize: "+plotName);
-        jf.setSize(500,400);
-        jf.getContentPane().setLayout(new BorderLayout());
-        jf.getContentPane().add(vmc, BorderLayout.CENTER);
-        jf.addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosing(java.awt.event.WindowEvent e) {
-                jf.dispose();
-            }
-        });
-        jf.setVisible(true);
+        /*   // generate curve
+	     ThresholdCurve tc = new ThresholdCurve();
+	     int classIndex = 0;
+	     Instances result = tc.getCurve(eval.predictions(), classIndex);
+         
+	     // plot curve
+	     ThresholdVisualizePanel vmc = new ThresholdVisualizePanel();
+	     vmc.setROCString("(Area under ROC = " +  Utils.doubleToString(tc.getROCArea(result), 4) + ")");
+	     vmc.setName(result.relationName());
+	     PlotData2D tempd = new PlotData2D(result);
+	     tempd.setPlotName(result.relationName());
+	     tempd.addInstanceNumberAttribute();
+	     // specify which points are connected
+	     boolean[] cp = new boolean[result.numInstances()];
+	     for (int n = 1; n < cp.length; n++)
+         cp[n] = true;
+	     tempd.setConnectPoints(cp);
+	     // add plot
+	     vmc.addPlot(tempd);
+         
+	     // display curve
+	     String plotName = vmc.getName();
+	     final javax.swing.JFrame jf =
+         new javax.swing.JFrame("Weka Classifier Visualize: "+plotName);
+	     jf.setSize(500,400);
+	     jf.getContentPane().setLayout(new BorderLayout());
+	     jf.getContentPane().add(vmc, BorderLayout.CENTER);
+	     jf.addWindowListener(new java.awt.event.WindowAdapter() {
+         public void windowClosing(java.awt.event.WindowEvent e) {
+         jf.dispose();
+         }
+	     });
+	     jf.setVisible(true);*/
     }
     
 }
