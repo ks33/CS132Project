1d0b9b726d12509589156a54809b6359b3740214
JessieFeng <jiaxi_feng@brown.edu>
2014-04-09 20:15:37 -0400

    runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -9,6 +9,27 @@ import java.util.*;
 public class Runner {
 
 	public static void main(String[] args) {
+        // 1) Change WekaProcessing main function's return type to Classifer, change name?
+		// 2) Make function return classifier
+		// c = (new WekaProcessing()).train()
+		// 3) create a new POST route (JsonTransformer)
+		// 			in this function, parse JSON from request body
+		//          create Instance() out of it (weka class)
+		//			pass it into c.classifyInstance()
+		//			create a Java class similar to MyMessage that will have instance variables for the attributes
+		//			of your response
+		//			return an instance of this class to generate a response
+		//	handle in client-side javascript
+		
+		c = (new WekaProcessing()).train();
+		
+		post(new JsonTransformerRoute("/train") {
+		    @Override
+		    public Object handle(Request request, Response response) {
+		    	
+                return ;
+		    }
+        });
 		// TODO Auto-generated method stub
 		get(new Route("/") {
 	         @Override
