2e074ac5331640426df40d4db01151018ef3d6d5
dav92lee <dav92lee@gmail.com>
2014-05-01 15:37:43 -0400

    hello

--- spark-demo2/src/main/java/BuildDB.java
+++ spark-demo2/src/main/java/BuildDB.java
@@ -1,10 +1,14 @@
 package main.java;
 
+import java.io.File;
 import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -16,24 +20,20 @@ public class BuildDB
 		
 		String CSV_FILE = "Data.csv";
 		String DB_FILE = "Database.db";
+		Scanner sc = new Scanner(new FileReader(new File("Data.csv")));
+		String attributes = "";
+		if(sc.hasNext())
+			attributes = sc.nextLine();
+		attributes = attributes.replaceAll("-", "_");
+		List<String> attributesList = Arrays.asList(attributes.split(","));
 		String createTableSQL = "CREATE TABLE LapseTable("
-				+ "ID int AUTOINCREMENT,"
-				+ "tenure DOUBLE PRECISION,"
-				+ "is30dPayer DOUBLE PRECISION,"
-				+ "paymentAmount30d DOUBLE PRECISION," 
-				+ "engmtLastWk DOUBLE PRECISION,"
-				+ "engmtWk_1 DOUBLE PRECISION,"
-				+ "engmtWk_2 DOUBLE PRECISION,"
-				+ "engmtWk_3 DOUBLE PRECISION,"
-				+ "engmt28d DOUBLE PRECISION,"
-				+ "deltaEngmt4wk DOUBLE PRECISION,"
-				+ "socialActionsReceivedLastWk DOUBLE PRECISION,"
-				+ "socialActorsReceivedLastWk DOUBLE PRECISION,"
-				+ "socialActionsSentLastWk DOUBLE PRECISION,"
-				+ "socialActorsSentLastWk DOUBLE PRECISION,"
-				+ "daysSinceLastSend DOUBLE PRECISION,"
-				+ "daysSinceLastReceive DOUBLE PRECISION,"
-				+ "trueLapse VARCHAR(50));";
+				+ "ID INTEGER PRIMARY KEY AUTOINCREMENT, ";
+		for (int i = 0; i < attributesList.size() - 1; i++)
+		{
+			createTableSQL = createTableSQL + attributesList.get(i) + " DOUBLE PRECISION, ";
+		}
+		createTableSQL = createTableSQL + attributesList.get(attributesList.size()-1) + " VARCHAR(50));";
+				System.out.println(createTableSQL);
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + DB_FILE);
 		
@@ -44,10 +44,18 @@ public class BuildDB
 		stat.executeUpdate("PRAGMA journal_mode = MEMORY;");
 		
 		
-		String insertdata = "INSERT OR IGNORE INTO LapseTable" +
-				" (tenure, is30dPayer, paymentAmount30d, engmtLastWk, engmtWk_1, engmtWk_2, engmtWk_3, engmt28d, deltaEngmt4wk, socialActionsReceivedLastWk, socialActorsReceivedLastWk, socialActionsSentLastWk, socialActorsSentLastWk, daysSinceLastSend, daysSinceLastReceive, trueLapse)" +
-				" VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		PreparedStatement prep = conn.prepareStatement(insertdata);
+		String values = "";
+		String attrString = "";
+		for (int i = 0; i < attributesList.size()-1; i++)
+		{
+			attrString = attrString + attributesList.get(i) + ", ";
+			values = values + "?, ";
+		}
+		attrString = attrString + attributesList.get(attributesList.size()-1);
+		values = values + "?";
+		String insertData = "INSERT OR IGNORE INTO LapseTable (" + attrString + ") VALUES (" + values+")";
+
+		PreparedStatement prep = conn.prepareStatement(insertData);
 		CSVReader reader = new CSVReader(new FileReader(CSV_FILE));
 		boolean firstline = true;
 		String [] nextLine;
@@ -56,24 +64,11 @@ public class BuildDB
 					nextLine = reader.readNext();
 					firstline = false;
 				}
-				 prep.setDouble(1, Double.parseDouble(nextLine[0]));
-				 prep.setDouble(2, Double.parseDouble(nextLine[1]));
-				 prep.setDouble(3, Double.parseDouble(nextLine[2]));
-				 prep.setDouble(4, Double.parseDouble(nextLine[3]));
-				 prep.setDouble(5, Double.parseDouble(nextLine[4]));
-				 prep.setDouble(6, Double.parseDouble(nextLine[5]));
-				 prep.setDouble(7, Double.parseDouble(nextLine[6]));
-				 prep.setDouble(8, Double.parseDouble(nextLine[7]));
-				 prep.setDouble(9, Double.parseDouble(nextLine[8]));
-				 prep.setDouble(10, Double.parseDouble(nextLine[9]));
-				 prep.setDouble(11, Double.parseDouble(nextLine[10]));
-				 prep.setDouble(12, Double.parseDouble(nextLine[11]));
-				 prep.setDouble(13, Double.parseDouble(nextLine[12]));
-				 prep.setDouble(14, Double.parseDouble(nextLine[13]));
-				 prep.setDouble(15, Double.parseDouble(nextLine[14]));
-				 prep.setString(16, nextLine[15]);
+				int i = 0;
+				for (i = 0; i < attributesList.size()-1; i++)
+					prep.setDouble(i+1, Double.parseDouble(nextLine[i]));
+				 prep.setString(i+1, nextLine[i]);
 				 prep.addBatch(); 	
-
 		 }
 		conn.setAutoCommit(false);
 		prep.executeBatch();
